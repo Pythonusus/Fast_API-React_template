22fe771060213afec36bab91c5698c1f7d022b21
Petr Malafeev <malafeev.pa@gmail.com>
2026-08-07 13:48:55 +0300

    awedasdfadf

--- frontend/app/config.ts
+++ frontend/app/config.ts
@@ -1,12 +1,13 @@
-import type { ThemeAppearance } from "~/types/theme";
-
 /*
 Frontend application configuration.
 Check the .env.example file in the root directory for more information.
 */
+
+import type { ThemeAppearance } from "~/types/theme";
 
 export const DEVELOPMENT =
   (import.meta.env.DEVELOPMENT ?? "").toLowerCase() === "true";
 
 export const THEME_STORAGE_KEY = "app-theme";
 export const DEFAULT_THEME: ThemeAppearance = "light";
+export const HTML_LANG = import.meta.env.HTML_LANG ?? "ru-RU";
